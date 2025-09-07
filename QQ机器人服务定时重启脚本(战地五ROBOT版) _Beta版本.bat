02808d37ac69f419d3c78d76a0e245dae5128d4a
Tian-Meng-Jun <143889343+Tian-Meng-Jun@users.noreply.github.com>
2025-09-08 01:23:59 +0800

    修复多个BUG和重写大部分注释

--- QQ机器人服务定时重启脚本(战地五ROBOT版) _Beta版本.bat
+++ QQ机器人服务定时重启脚本(战地五ROBOT版) _Beta版本.bat
@@ -1,19 +1,23 @@
 @echo off
 setlocal enabledelayedexpansion
-TITLE  QQ机器人服务定时重启脚本(战地五ROBOT版) v9.2.5_Beta
-REM 设置ROBOT和QQ的存放路径
-set "ROBOT_Path=C:\Program Files Green"
+TITLE  QQ机器人服务定时重启脚本(战地五ROBOT版) v9.2.6_Beta
+
+
+REM 脚本配置
+:: |设置ROBOT和QQ的存放路径|
+set "ROBOT_Path=C:\Program Files\Catclient"
 set "QQ_Path=C:\Program Files\Tencent\QQNT"
-REM 设置ROBOT和QQ的启动文件
+:: |设置ROBOT和QQ的启动文件|
 set "ROBOT_File=catclient.exe"
 set "QQ_File=QQ.exe"
-REM 设置脚本的启动与结束时间范围 (格式 HHMM)
+:: |设置脚本的启动与结束时间范围(格式:HHMM)|
 set "start_time=1200"
 set "end_time=0200"
-REM 设置QQ多长时间重启
+:: |设置QQ多长时间重启|
 set Gap=6100
 
-:: 功能：选项页面
+
+REM 选项页面
 :UI
 cls
 echo. 
@@ -27,23 +31,25 @@ echo.     2. 指定时间范围内循环       (结束后不保留ROBOT程序)
 echo. 
 echo.     3. 指定次数范围内循环        (结束后保留ROBOT程序)
 echo. 
-echo.     4. 指定次数范围内循环        (结束后不保留ROBOT程序)
+echo.     4. 指定次数范围内循环       (结束后不保留ROBOT程序)
 echo. 
-echo.     5. 二十四小时一直循环         (需手动结束ROBOT程序)
+echo.     5. 二十四小时一直循环        (需手动结束ROBOT程序)
 echo. 
 echo.     6. 只启动[BFV ROBOT]         (需手动结束ROBOT程序)
 echo. 
 echo. =========================================================
+
 choice /c 123456 /n /M "请输入要执行的选项："
 set "Figure=%errorlevel%"
 if %errorlevel%==1 goto Time_to_Run
 if %errorlevel%==2 goto Time_to_Run
 if %errorlevel%==3 goto Loop_to_Run
 if %errorlevel%==4 goto Loop_to_Run
-if %errorlevel%==5 goto 24H
-if %errorlevel%==6 goto ROBOT  
+if %errorlevel%==5 goto Run_5
+if %errorlevel%==6 goto Run_6  
 
-:: 方案跳转分区
+
+REM 方案跳转分区:
 :Time_to_Run
 cls
 call :Time
@@ -63,21 +69,24 @@ set /p count=请输入要循环的次数(必须为正整数)：
 goto Run_%Figure%
 
 
-:: 方案执行分区
+REM 方案执行分区:
 
-:: 指定时间范围内循环 (结束后不保留ROBOT程序)
+::  1.指定时间范围内循环(结束后保留ROBOT程序)
+::  ===========================================================
 :Run_1
 cls
 call :Loop_1
 goto Time_to_Run
 
-:: 指定时间范围内循环 (结束后保留ROBOT程序)
+::  2.指定时间范围内循环(结束后不保留ROBOT程序)
+::  ===========================================================
 :Run_2
 cls
-call :Loop_1
+call :Loop_2
 goto Time_to_Run
 
-:: 指定次数循环 (结束后不保留ROBOT程序)
+::  3.指定次数循环 (结束后保留ROBOT程序)
+::  ===========================================================
 :Run_3
 for /l %%i in (1,1,%count%) do (
     cls
@@ -86,7 +95,8 @@ for /l %%i in (1,1,%count%) do (
 )
 goto UI
 
-:: 指定次数循环 (结束后保留ROBOT程序)
+::  4.指定次数循环 (结束后不保留ROBOT程序)
+::  ===========================================================
 :Run_4
 for /l %%i in (1,1,%count%) do (
     cls
@@ -95,26 +105,37 @@ for /l %%i in (1,1,%count%) do (
 )
 goto UI
 
-:: 功能模块
-
-:: 指定次数循环 (结束后保留ROBOT程序)
-:24H
+::  5.二十四小时一直循环
+::  ===========================================================
+:Run_5
 cls
 call :Loop_2
-goto 24H
+goto Run_5
 
-:: 获取当前时间
+::  6.只启动[BFV ROBOT]
+::  ===========================================================
+:Run_6
+cls
+call :ROBOT
+timeout /t 2
+goto UI
+
+
+REM 功能模块:
+
+::  获取当前时间和转换时间格式
+::  ===========================================================
 :Time
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
 set current_hour=!datetime:~8,2!
 set current_minute=!datetime:~10,2!
-:: 将当前时间转换为分钟数以便比较
 set /a current_time_minutes=(!current_hour!*60)+!current_minute!
 set /a start_time_minutes=(%start_time:~0,2%*60)+%start_time:~2,2%
 set /a end_time_minutes=(%end_time:~0,2%*60)+%end_time:~2,2%
 exit /b
 
-:: 结束后保留ROBOT程序
+::  结束后保留ROBOT程序
+::  ===========================================================
 :Loop_1
 call :Kill
 timeout /t 6
@@ -125,7 +146,8 @@ timeout /t %Gap%
 taskkill /IM QQ.exe /F
 exit /b
 
-:: 结束后不保留ROBOT程序
+::  结束后不保留ROBOT程序
+::  ===========================================================
 :Loop_2
 call :Kill
 timeout /t 6
@@ -136,13 +158,15 @@ timeout /t %Gap%
 call :Kill
 exit /b
 
-:: ROBOT程序启动
+::  ROBOT程序启动
+::  ===========================================================
 :ROBOT
 start "" /D "%ROBOT_Path%" /MIN "%ROBOT_File%"
 cls
 exit /b
 
-:: 结束ROBOT程序与QQ程序
+::  结束ROBOT程序与QQ程序
+::  ===========================================================
 :Kill
 taskkill /IM QQ.exe /F
 taskkill /IM %ROBOT_File%
